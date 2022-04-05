b7b3879e9848803c3cb723930f1e2c67464fd3d6
manoj <manojreddy.a2@gmail.com>
2022-04-05 17:26:48 +0530

    moved full_name file to chapter2 and created apostrophe.py file in chapter_02

---
+++ chapter_02/apostrophe.py
@@ -0,0 +1,24 @@
+message = "One of python's strength is its diverse community"
+print(message)
+
+#syntax error
+#message = 'One of Python's strengths is its diverse community.'
+#print(message)
+
+personal_message = "Hello manoj, would you like to learn some Python today?"
+person_name = 'manoj reddy'
+print(person_name.upper())
+print(person_name.lower())
+print(person_name.title())
+
+quote = 'Albert Einstein once said, "A person who never made a mistake never tried anything new."'
+print(quote)
+
+famous_person = 'Lao Tzu'
+famous_quote = '"The journey of a thousand miles begins with one step"'
+
+famous_message = f"{famous_person}, {famous_quote}"
+print(famous_message)
+
+name = ' john abraham '
+print(f"\t{name}\n{name.rstrip()}\n{name.lstrip()}\n{name.strip()}")
