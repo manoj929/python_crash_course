c37f0b04f3752ce78d9871f5655e012d8ef7b82a
manoj <manojreddy.a2@gmail.com>
2022-04-05 19:37:03 +0530

    created excercise.py file in chapter_03

---
+++ chapter_03/excercise.py
@@ -0,0 +1,18 @@
+countries = ['usa', 'australia', 'thailand', 'newzland', 'dubai']
+print(countries)
+print(sorted(countries))
+print(countries)
+countries.reverse()
+print(countries)
+countries.reverse()
+print(countries)
+countries.sort()
+print(countries)
+countries.sort(reverse=True)
+print(countries)
+
+friends = ['raju', 'naveen', 'sreenu', 'sumanth', 'rajdj', 'nagraj']
+print(f"these {len(friends)} persons are friends from childhood")
+#storing list length in a variable
+people = len(friends)
+print(people)
