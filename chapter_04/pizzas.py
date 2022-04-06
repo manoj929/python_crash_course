2a3122cd7cae86285e34badd2ae96602a7c232cb
manoj <manojreddy.a2@gmail.com>
2022-04-06 13:57:44 +0530

    created pizzas file and cheges in magicians file

---
+++ chapter_04/pizzas.py
@@ -0,0 +1,12 @@
+pizzas = ['chicken cheese', 'mexican', 'pepperOnion']
+for pizza in pizzas:
+    print(f"i like {pizza.title()}")
+
+print(f"{pizzas[0].title()} is so yummy")
+print('i like it very much and always want to eat')
+print('i like pizza')
+
+animals = ['lion', 'tiger', 'leopard']
+for animal in animals:
+    print(f"{animal.title()} is a wild animal")
+print(f"any of these animals could make harm to humans!")
