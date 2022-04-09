d36d1dd859a41f6cc2770124fe9e56b896baa1b4
manoj <manojreddy.a2@gmail.com>
2022-04-09 15:30:24 +0530

    make changes in alien.py file

--- chapter_06/alien.py
+++ chapter_06/alien.py
@@ -12,3 +12,32 @@ print(alien_0)
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
+
+#starting with an empty dictionary
+alien_1 = {}
+
+alien_1['color'] = 'yellow'
+alien_1['points'] = 10
+print(alien_1)
+print()
+#modifying values in dictionary
+print(f"The allien is {alien_1['color']}")
+alien_1['color'] = 'red'
+print(f"The allien is now {alien_1['color']}")
+print()
+#moving alien
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_0['x_position']}")
+
+#moving alien to the right
+#determine how far to move the alien based on its current speed
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # this must be fast alien
+    x_increment = 3
+# the new position is old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
