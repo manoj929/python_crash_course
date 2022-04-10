f13527ad91687f25b3043b890140d697c7503bd4
manoj <manojreddy.a2@gmail.com>
2022-04-10 17:59:12 +0530

    changes in favorite_languages.py file

--- chapter_06/favourite_languages.py
+++ chapter_06/favourite_languages.py
@@ -7,3 +7,16 @@ favorite_languages = {
 }
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is: {language}.")
+print()
+
+#loop through dictionary
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is: {language.title()}")
+
+#looping through all the keys in a dictionary (keys() easier to read)
+for name in favorite_languages.keys():
+    print(f"\n{name.title()}")
+
+# or use any of one both works same for (displays keys only)
+for name in favorite_languages:
+    print(name.title())
