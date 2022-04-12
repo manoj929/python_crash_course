885ee5f5102d2a43cb8613667106f8491575c79d
manoj <manojreddy.a2@gmail.com>
2022-04-12 12:34:09 +0530

    created chapter_07 and input function files

---
+++ chapter_07/greeter.py
@@ -0,0 +1,5 @@
+#multi line message
+prompt = 'if you tell us who you are , we can personalize the message you see.'
+prompt += '\nwhat is your first name? '
+name = input(prompt)
+print(f"\nHello {name}")
---
+++ chapter_07/parrot.py
@@ -0,0 +1,7 @@
+#input() method
+message = input('Tell me something, and I will repeat it back  to you: ')
+print(message)
+
+#writing clear prompts
+name = input('please enter you name: ')
+print(f"\nHello {name}!")
