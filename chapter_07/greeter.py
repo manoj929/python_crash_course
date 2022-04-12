720ec9c8002a176585980fbee6d2d88a4a7d4f58
manoj <manojreddy.a2@gmail.com>
2022-04-12 15:18:05 +0530

    created rollercoaster.py file

--- chapter_07/greeter.py
+++ chapter_07/greeter.py
@@ -3,3 +3,11 @@ prompt = 'if you tell us who you are , we can personalize the message you see.'
 prompt += '\nwhat is your first name? '
 name = input(prompt)
 print(f"\nHello {name}")
+
+age = input("Hello what is your age? ")
+print(age)
+print(type(age))
+
+age = int(age)
+print(type(age))
+print(age >= 18)
---
+++ chapter_07/rollercoaster.py
@@ -0,0 +1,7 @@
+# convert and check condition
+height = input("How tall are you, in inches? ")
+height = int(height)
+if height >= 48:
+    print("\nyou're tall enough to ride!")
+else:
+    print("\nyou'll be able to ride when you are a little older.")
