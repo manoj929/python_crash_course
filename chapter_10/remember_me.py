8150364c876f1b33df63ed507beb218367bbd47b
manoj <manojreddy.a2@gmail.com>
2022-04-21 22:17:24 +0530

    combined storing and retrieving data in one file

--- chapter_10/remember_me.py
+++ chapter_10/remember_me.py
@@ -1,8 +1,15 @@
 import json
 
-username = input("What is your name? ")
-
+#load the username, if it has been stored previously.
+# otherwise, primpt for the username and store it.
 filename = "chapter_10/username.json"
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f"we'll remember you when you come back, {username}")
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"we'll remember you when you come back, {username}")
+else:
+    print(f"welcome back {username}")
