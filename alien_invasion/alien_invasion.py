7f6a84b57ee1dc1079ea222acfd3892d58bd9516
manoj <manojreddy.a2@gmail.com>
2022-05-13 14:45:05 +0530

    changes bullet functionality

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -33,7 +33,7 @@ class AlienInvasion:
             # watch for keyboard and mouse events.
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
             # redraw  the screen during each pass through the loop.
@@ -69,8 +69,18 @@ class AlienInvasion:
     
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
     
     def _update_screen(self):
         """update images on the screen, and flip to the new screen."""
